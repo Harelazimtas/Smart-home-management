55a79e12099bc9daaf481b3839e07f0195ef0066
avi avi <harelazimm1@gmail.com>
2021-07-26 18:20:25 +0300

    addmission

--- app/src/main/java/com/minesweeper/smart_home_management/MainActivity.java
+++ app/src/main/java/com/minesweeper/smart_home_management/MainActivity.java
@@ -2,7 +2,10 @@ package com.minesweeper.smart_home_management;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start_button = findViewById(R.id.button1);
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openAddMissioncreen();
+            }
+
+
+        });
     }
+
+    private  void openAddMissioncreen(){
+        Intent intent=new Intent(this,MissionAdd.class);
+        startActivity(intent);
+    }
+
+
+
 }
